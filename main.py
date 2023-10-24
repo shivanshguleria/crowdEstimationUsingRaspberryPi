fc068409310d72d98bd5a48fb49ecc417ba4a8d8
shivansh <shivansh.guleria@yahoo.com>
2023-10-24 13:24:23 +0530

    commit

---
+++ main.py
@@ -0,0 +1,161 @@
+
+#python beforeLED.py -c true
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import imutils
+import cv2
+import time
+import argparse
+import base64
+
+
+#For led module
+from signal import signal, SIGTERM, SIGHUP, pause
+from rpi_lcd import LCD
+lcd = LCD()
+def safe_exit(signum, frame):
+    exit(1)
+
+
+DEVICE = "detector"  # Device where will be stored the result
+VARIABLE = "people"  # Variable where will be stored the result
+
+# Opencv pre-trained SVM with HOG people features 
+HOGCV = cv2.HOGDescriptor()
+HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+def detector(image):
+    '''
+    @image is a numpy array
+    '''
+
+    clone = image.copy()
+
+    (rects, weights) = HOGCV.detectMultiScale(image, winStride=(4, 4),
+                                              padding=(8, 8), scale=1.05)
+
+    # draw the original bounding boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(clone, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # Applies non-max supression from imutils package to kick-off overlapped
+    # boxes
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    result = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    return result
+
+
+def buildPayload(variable, value, context):
+    return {variable: {"value": value, "context": context}}
+
+
+
+def argsParser():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", default=None,
+                    help="path to image test file directory")
+    ap.add_argument("-c", "--camera", default=False,
+                    help="Set as true if you wish to use the camera")
+    args = vars(ap.parse_args())
+
+    return args
+
+
+def localDetect(image_path):
+    result = []
+    image = cv2.imread(image_path)
+    image = imutils.resize(image, width=min(400, image.shape[1]))
+    clone = image.copy()
+    if len(image) <= 0:
+        print("[ERROR] could not read your local image")
+        return result
+    print("[INFO] Detecting people")
+    result = detector(image)
+
+    # shows the result
+    for (xA, yA, xB, yB) in result:
+        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    cv2.imshow("result", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cv2.imwrite("result.png", np.hstack((clone, image)))
+    return (result, image)
+
+
+def cameraDetect(variable, sample_time=5):
+
+    cap = cv2.VideoCapture(0)
+    init = time.time()
+
+    # Allowed sample time for Ubidots is 1 dot/second
+    if sample_time < 1:
+        sample_time = 1
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+        result = detector(frame.copy())
+
+        # shows the result
+        for (xA, yA, xB, yB) in result:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        cv2.imshow('frame', frame)
+
+        # Sends results
+        if time.time() - init >= sample_time:
+            print("[INFO] Sending actual frame results")
+            # Converts the image to base 64 and adds it to the context
+            try:
+                signal(SIGTERM, safe_exit)
+                signal(SIGHUP, safe_exit)
+                print("[INFO] Printing on LCD Screen")
+                lcd.text("Number of people:-", 1)
+                lcd.text(f"{len(result)}", 2)
+                pause()
+            except KeyboardInterrupt:
+                pass
+            finally:
+                lcd.clear()
+
+            print(len(result))
+            init = time.time()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def convert_to_base64(image):
+    image = imutils.resize(image, width=400)
+    img_str = cv2.imencode('.png', image)[1].tobytes()
+    b64 = base64.b64encode(img_str)
+    return b64.decode('utf-8')
+
+
+def detectPeople(args):
+    image_path = args["image"]
+    camera = True if str(args["camera"]) == 'true' else False
+
+    # Routine to read local image
+    if image_path != None and not camera:
+        print("[INFO] Image path provided, attempting to read image")
+        (result, image) = localDetect(image_path)
+        print("[INFO] sending results")
+    if camera:
+        print("[INFO] reading camera images")
+        cameraDetect(VARIABLE)
+
+
+def main():
+    args = argsParser()
+    detectPeople(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.txt
@@ -0,0 +1,23 @@
+###Commands
+
+  1.  source Scripts/activate
+            Activate virtual env
+  2.  tar zxvf dependencies.tar.gz
+            Unpack dependencies
+  3.  cd Dependencies
+            Change Directory
+  4.  pip install * -f ./ --no-index
+            Install Packages
+  5.  cd.. 
+            change Directory
+  6.  python main.py -c true
+            Run main file 
+  7.  deactivate
+            quite virtualenv
+
+###Useful links
+
+
+Connecting lcd module to Pi
+
+https://www.electroniclinic.com/raspberry-pi-16x2-lcd-i2c-interfacing-and-python-programming/
